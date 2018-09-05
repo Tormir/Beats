8272975ea917862eeb7676801b3957c935a91b81
KM <kmacew@gmail.com>
2018-09-05 18:17:58 +0200

    Add generated sysusage files

--- main.go
+++ main.go
@@ -3,16 +3,14 @@ package main
 import (
 	"os"
 
-	"github.com/KM/sysusage/cmd"
-//	"github.com/elastic/beats/libbeat/beat"
+	"github.com/elastic/beats/libbeat/beat"
 	"github.com/KM/sysusage/beater"
-	_ "github.com/KM/sysusage/include"
+//	_ "github.com/KM/sysusage/include"
 )
 
-var RootCmd = cmd.GenRootCmd("sysusage", "", beater.New)
-
 func main() {
-	if err := cmd.RootCmd.Execute(); err != nil {
+	err := beat.Run("sysusage", "", beater.New)
+	if err != nil {
 		os.Exit(1)
 	}
 }
